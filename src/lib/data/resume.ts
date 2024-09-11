673f858d35802cb3d3000ac33d6e47ff2814e3e2
Ash Zahabiuon <ash@mountaininteractive.com>
2024-09-11 10:24:12 -0400

    resume

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -1,3 +1,3 @@
-export const data = '';
+export const data = 'https://raw.githubusercontent.com/MrScruffles/ashz/main/static/screenshots/resume/Ash_Zahabiuon_2024_RESUME.pdf';
 
 export const title = 'Resumé';
