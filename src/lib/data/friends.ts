331c95454a9e7ddc45cfffa020bf2329ed114474
Ash Zahabiuon <83662366+MrScruffles@users.noreply.github.com>
2025-10-18 16:08:53 -0400

    Update friends.ts

--- src/lib/data/friends.ts
+++ src/lib/data/friends.ts
@@ -6,7 +6,7 @@ export const items: Array<Friend> = [
 		slug: 'friend-1',
 		name: 'Error Cezar',
 		logo: Assets.Unknown,
-		shortDescription: 'Software Engineer',
+		shortDescription: 'Developer',
 		description: 'well he knows that cats aren\'t real at least.',
 		links: [
 			{ to: 'https://github.com/Error-Cezar', label: 'GitHub' },
