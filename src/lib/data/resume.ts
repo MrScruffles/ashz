3e0b46ac6601eddfcfbd3a9afcdac7e9f577675c
Ash Zahabiuon <83662366+MrScruffles@users.noreply.github.com>
2026-09-24 00:39:12 -0400

    cat

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -1,3 +1,3 @@
-export const data = 'https://raw.githubusercontent.com/MrScruffles/ashz/main/static/screenshots/resume/Ashkan_Zahabiuon_Jenkins_Resume.pdf';
+export const data = 'https://raw.githubusercontent.com/MrScruffles/ashz/main/static/screenshots/resume/JenkinsMBA_Ashkan_Zahabiuon_Resume.pdf';
 
 export const title = 'Resumé';
